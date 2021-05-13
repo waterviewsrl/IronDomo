0872230f7f88e332e846cb5264f47b08b593849b
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2021-05-13 16:36:07 +0200

    Tagging v1.1.7

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='IronDomo',
-      version='1.1.6',
+      version='1.1.7',
       author='Matteo Ferrabone',
       author_email='matteo.ferrabone@gmail.com',
       packages=find_packages(),
