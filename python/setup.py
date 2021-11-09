188c797ffd7e0f413702e391ae0b873701d0af9f
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2021-11-09 09:43:08 +0100

    Bump version to 1.2.1

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='IronDomo',
-      version='1.2.0',
+      version='1.2.1',
       author='Matteo Ferrabone',
       author_email='matteo.ferrabone@gmail.com',
       packages=find_packages(),
